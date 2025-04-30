ab75623d794b07f4a2565070808998a9202e2e5b
tiken12 <wikenderalcius12@gmail.com>
2025-04-29 21:33:17 -0600

    Average time per iteration

--- daxpy_benchmark.py
+++ daxpy_benchmark.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.linalg.blas import daxpy
 import time
 
-def daxpy_benchmark(N, alpha=2.0):
+def daxpy_benchmark(N, alpha=2.0, repeat=1000):
     x = np.ones(N, dtype=np.float64)
     y = np.ones(N, dtype=np.float64)
 
@@ -10,16 +10,22 @@ def daxpy_benchmark(N, alpha=2.0):
     _ = daxpy(x, y, a=alpha)
 
     start = time.perf_counter()
-    result = daxpy(x, y, a=alpha)
+    for _ in range(repeat):
+        _ = daxpy(x, y, a=alpha)
     end = time.perf_counter()
 
     elapsed = end - start
+    avg_elapsed = elapsed / repeat
     bytes_moved = x.nbytes + y.nbytes
-    bandwidth = bytes_moved / elapsed / 1e9
+    bandwidth = bytes_moved / avg_elapsed / 1e9
 
-    print(f"N = {N:,}")
-    print(f"Elapsed time: {elapsed:.4f} sec")
+    print(f"N = {N:,}, repeat= {repeat}")
+    print(f"Total elapsed time: {elapsed: .6f} sec")
+    print(f"Avg time per DAXPY: {avg_elapsed:.8f} sec")
     print(f"Bandwidth: {bandwidth:.2f} GB/s")
+
+    return avg_elapsed, bandwidth
 
 if __name__ == "__main__":
     daxpy_benchmark(100_000_000)
+
